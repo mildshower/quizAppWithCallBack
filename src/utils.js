b7acec894f74bc8ca9cb6e50afc7a32d27d603c5
Sudipta Kundu <58025792+mildshower@users.noreply.github.com>
2019-12-06 14:52:50 +0530

    First question is shown

--- src/utils.js
+++ src/utils.js
@@ -1,23 +1,5 @@
 const { stdin, stdout } = process;
 
-const intervalCall = function(callback, time, count, ...args) {
-  let counter = 0;
-  return new Promise((resolve, reject) => {
-    const timer = setInterval(() => {
-      try {
-        callback(...args);
-      } catch (err) {
-        reject(err);
-      }
-      counter++;
-      if (counter == count) {
-        resolve(true);
-        clearInterval(timer);
-      }
-    }, time);
-  });
-};
-
 const spaces = function(num) {
   return " ".repeat(num >= 0 ? num : 0);
 };
@@ -33,8 +15,4 @@ const clearScreen = function() {
   stdout.clearScreenDown();
 };
 
-const pause = async function(times) {
-  await intervalCall(() => {}, times, 1);
-};
-
-module.exports = { intervalCall, spaces, printAt, clearScreen, pause };
+module.exports = { spaces, printAt, clearScreen };
